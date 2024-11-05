f042ccf56c9192797f9a39de6c36d67c2c63b073
Alexej-Mich61 <spas-expert@yandex.ru>
2024-11-06 02:23:55 +0300

    D_2 сделал первое представление

--- todo/settings.py
+++ todo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todos' # добавляем приложение todos в список
 ]
 
 MIDDLEWARE = [
