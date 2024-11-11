5debb9c7a9c22db5b4ed88a437168e614a511214
Alexej-Mich61 <spas-expert@yandex.ru>
2024-11-11 04:07:39 +0300

    D_2 include, шаблон задач, представление, счет задач, пользователей на странице

--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import Task
 from .forms import TaskForm
 
 @login_required
 def task_list(request):
     tasks = Task.objects.all()
-    return render(request, 'todos/task_list.html', {'tasks': tasks})
+    users_count = User.objects.count()
+    return render(request, 'todos/task_list.html', {'tasks': tasks, 'users_count': users_count})
 
 @login_required
 def task_create(request):
